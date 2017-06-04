6516f1af3de1b7ce60ec6d257f05c52167d10b63
Prem Kumar <premkumar1989@gmail.com>
2017-06-04 13:11:49 +0530

    global error handler

--- server.js
+++ server.js
@@ -7,11 +7,18 @@ port = 54321;
 
 
 app.use(bodyParser.json());
-
 app.use(bodyParser.urlencoded({ extended: true }));
+
 
 var routes = require('./routes');
 routes(app);
+
+app.use(function(err, req, res, next) {
+  // Do logging and user-friendly error message display
+  console.error(err);
+  res.status(500).send({status:500, message: 'internal error', type:'internal'});
+});
+
 
 app.listen(port);
 
